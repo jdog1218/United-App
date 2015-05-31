2969e754b81f02221956689a63b77fe3f565bae4
Joel Gordon <jgordon17@outlook.com>
2015-05-30 21:44:14 -0700

    Sotd edit

--- app/src/main/java/update/app/jdog1218/com/messingaround/Sotd.java
+++ app/src/main/java/update/app/jdog1218/com/messingaround/Sotd.java
@@ -4,11 +4,12 @@ import android.app.Activity;
 
 import org.jsoup.nodes.Document;
 
-/**
+/*
  * Created by Joel on 5/28/2015.
  */
 public class Sotd extends Activity {
     String url = "https://www.compasshb.com/read";
     String html = null;
     Document doc = null;
+
 }
