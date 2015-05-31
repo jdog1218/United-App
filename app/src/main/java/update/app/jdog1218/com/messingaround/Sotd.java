c0de7bdda0e24edb0f6ccf3abf0d155bee945633
Joel Gordon <jgordon17@outlook.com>
2015-05-30 23:55:47 -0700

    SOTD Document called

--- app/src/main/java/update/app/jdog1218/com/messingaround/Sotd.java
+++ app/src/main/java/update/app/jdog1218/com/messingaround/Sotd.java
@@ -2,14 +2,31 @@ package update.app.jdog1218.com.messingaround;
 
 import android.app.Activity;
 
+import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+
+import static org.jsoup.Jsoup.*;
 
 /*
  * Created by Joel on 5/28/2015.
  */
 public class Sotd extends Activity {
-    String url = "https://www.compasshb.com/read";
-    String html = null;
-    Document doc = null;
 
+    public Document pullHTML() {
+        String url = "https://www.compasshb.com/read";
+        String value = null;
+        Document doc = null;
+        Elements HrefElse = null;
+
+        try {
+            doc = Jsoup.connect(url).get();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
