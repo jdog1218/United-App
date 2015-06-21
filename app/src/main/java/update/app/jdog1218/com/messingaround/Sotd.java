7a9ceae25721f94e33dd071e58054b7e32f68dee
Joel Gordon <jgordon17@outlook.com>
2015-06-20 23:15:17 -0700

    Error when calling Json from internet

--- app/src/main/java/update/app/jdog1218/com/messingaround/Sotd.java
+++ app/src/main/java/update/app/jdog1218/com/messingaround/Sotd.java
@@ -1,89 +1,135 @@
 package update.app.jdog1218.com.messingaround;
 
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-import java.util.List;
+import android.app.Activity;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+import android.os.AsyncTask;
 
 import org.apache.http.HttpEntity;
 import org.apache.http.HttpResponse;
-import org.apache.http.NameValuePair;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.entity.UrlEncodedFormEntity;
-import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
-import org.apache.http.client.utils.URLEncodedUtils;
 import org.apache.http.impl.client.DefaultHttpClient;
-import org.apache.http.util.EntityUtils;
+import org.apache.http.params.BasicHttpParams;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.select.Elements;
 
-public class Sotd {
+public class Sotd extends Activity {
 
-    static String response = null;
-    public final static int GET = 1;
-    public final static int POST = 2;
+    static String url = "https://www.compasshb.com/api/v1/passages";
+
+    static String stockSymbol;
+    static String stockTitle;
+    static String stockBody;
+    static String verse;
+    static String end;
+
+    MyAsyncTask asyncTask;
+
 
     public Sotd() {
 
     }
 
-    /**
-     * Making service call
-     *
-     * @url - url to make request
-     * @method - http request method
-     */
-    public String makeServiceCall(String url, int method) {
-        return this.makeServiceCall(url, method, null);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.sotd);
+
+        new MyAsyncTask().doInBackground();
     }
 
-    /**
-     * Making service call
-     *
-     * @url - url to make request
-     * @method - http request method
-     * @params - http request params
-     */
-    public String makeServiceCall(String url, int method,
-                                  List<NameValuePair> params) {
-        try {
-            // http client
-            DefaultHttpClient httpClient = new DefaultHttpClient();
-            HttpEntity httpEntity = null;
-            HttpResponse httpResponse = null;
+    public class MyAsyncTask extends AsyncTask<String, String, String> {
+        protected String doInBackground(String... Prams) {
 
-            // Checking http request method type
-            if (method == POST) {
-                HttpPost httpPost = new HttpPost(url);
-                // adding post params
-                if (params != null) {
-                    httpPost.setEntity(new UrlEncodedFormEntity(params));
+            DefaultHttpClient httpclient = new DefaultHttpClient(new BasicHttpParams());
+
+            HttpPost httppost = new HttpPost(url);
+
+            httppost.setHeader("Content-type", "application/json");
+
+            InputStream inputStream = null;
+
+            String result = null;
+
+            try {
+
+                HttpResponse response = httpclient.execute(httppost);
+                HttpEntity entity = response.getEntity();
+                inputStream = entity.getContent();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream, "UTF-8"), 8);
+                StringBuilder theStringBuilder = new StringBuilder();
+
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+
+                    theStringBuilder.append(line + "\n");
                 }
-
-                httpResponse = httpClient.execute(httpPost);
-
-            } else if (method == GET) {
-                // appending params to url
-                if (params != null) {
-                    String paramString = URLEncodedUtils
-                            .format(params, "utf-8");
-                    url += "?" + paramString;
+                result = theStringBuilder.toString();
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    if (inputStream != null) inputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
                 }
-                HttpGet httpGet = new HttpGet(url);
-
-                httpResponse = httpClient.execute(httpGet);
-
             }
-            httpEntity = httpResponse.getEntity();
-            response = EntityUtils.toString(httpEntity);
 
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
+            JSONObject jsonObject;
+
+            try {
+                //Log.v("JsonParser Result ", result);
+
+                jsonObject = new JSONObject(result);
+
+                JSONObject jsonTitle = jsonObject.getJSONObject("title");
+
+                stockTitle = jsonTitle.getString("title");
+
+                JSONObject html = jsonObject.getJSONObject("verses");
+
+                stockBody = html.getString("verses");
+
+                Document doc = Jsoup.parse(stockBody);
+
+                Elements body = doc.getElementsByClass("esv");
+
+                stockBody = body.text();
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return result;
         }
 
-        return response;
 
+        @Override
+        protected void onPostExecute(String result) {
+            TextView title = (TextView) findViewById(R.id.text_view);
+            TextView body = (TextView) findViewById(R.id.body);
+
+            title.setText("Title: " + stockTitle);
+            body.setText("body: " + stockBody);
+        }
     }
+
+    public String makeServiceCall(String url, int method) {
+
+
+        return this.makeServiceCall(url, method);
+    }
+
+
 }
